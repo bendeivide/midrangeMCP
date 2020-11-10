a62bd1d44344b4ba481ebbde1b0bbca2b02fc6e6
bendeivide <ben.deivide@gmail.com>
2020-11-10 20:43:48 -0300

    Update travis => tcltk

--- comandos_importantes.R
+++ comandos_importantes.R
@@ -22,6 +22,17 @@ usethis::use_build_ignore()
 # Update cran-comments.md
 usethis::use_cran_comments()
 
+# Versao do pacote
+usethis::use_release_issue()
+# Current version is 3.2.
+# Which part to increment? (0 to exit)
+#
+# 1: major --> 4.0
+# 2: minor --> 3.3
+# 3: patch --> 3.2.1
+# 4:   dev --> 3.2.0.9000
+
+
 
 
 
